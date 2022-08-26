0a7436a2acf3a3ca51226f946a42f16b77df4e76
Piotr Maciejewski <pm@piotrs-macbook-pro.home>
2022-08-26 17:07:01 +0200

    update_aug22

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,35 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Intro from '../components/Intro'
-import Navbar from '../components/Navbar'
-import styled from 'styled-components'
-import Services from '../components/Services'
-import News from '../components/News'
-import HeliumIntro from '../components/HeliumIntro'
-
+import Head from "next/head";
+import Image from "next/image";
+import Intro from "../components/Intro";
+import Navbar from "../components/Navbar";
+import styled from "styled-components";
+import Services from "../components/Services";
+import News from "../components/News";
+import HeliumIntro from "../components/HeliumIntro";
 
 const Container = styled.div`
   display: flex;
-  
+
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  
-`
-
+`;
 
 export default function Index() {
-  
   return (
     <>
       <Navbar />
       <Container>
-          
-          <Head>
-            <title></title>
-            <meta name="description" content="Helium Blog" />
-            <link rel="icon" href="#" /> 
-          </Head>
+        <Head>
+          <title></title>
+          <meta name="description" content="Helium Blog" />
+          <link rel="icon" href="#" />
+        </Head>
         <Intro />
-        <HeliumIntro />
+
         <News />
-        
+        <HeliumIntro />
       </Container>
     </>
-  )
+  );
 }
